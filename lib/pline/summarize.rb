eccd112855e771b9f7c25aa9e7403d5c1853156a
Satoshi Shiba <shiba@rvm.jp>
2012-02-18 03:00:40 +0900

    make result message pretty

--- lib/pline/summarize.rb
+++ lib/pline/summarize.rb
@@ -6,6 +6,62 @@ module PLine
       @@output = io
     end
 
+    def build_message(desc, labels, contents, pretty = true)
+      results = []
+      l_desc = desc.length
+      column_size = labels.size
+      contents = contents.inject([]) do |ary, c|
+        bug() unless c.size == column_size
+        c.map! do |v|
+          v.split("\n")
+        end
+        max = c.inject(0) do |m, v|
+          l = v.size
+          m > l ? m : l
+        end
+        max.times do |i|
+          ary << c.map{|v| v[i] || ''}
+        end
+        ary
+      end
+      l_labels = contents.inject(labels.map{|t| t.length}) do |a0, a1|
+        bug() unless a0.size == a1.size
+        a0.zip(a1).map do |(v0, v1)|
+          length = v1.length
+          v0 > length ? v0 : length
+        end
+      end
+      title = labels.zip(l_labels).map{|(t, l)| t.center(l)}.join(" | ")
+      l_title = title.length
+      width = l_desc > l_title ? l_desc : l_title
+      if width != l_title
+        bonus = width - l_title
+        adjust = column_size - bonus % column_size
+        width += adjust
+        bonus += adjust
+        bonus /= column_size
+        l_labels.map!{|l| l + bonus}
+        title = labels.zip(l_labels).map{|(t, l)| t.center(l)}.join(" | ")
+      end
+      width += 2
+      sep = "-" * width
+      results << " #{sep} "
+      results << "|#{desc.center(width)}|"
+      results << "|#{sep}|"
+      results << "|#{title.center(width)}|"
+      results << "|#{sep}|"
+      if pretty
+        side = "|"
+      else
+        side = ""
+      end
+      contents.each do |line|
+        results << "#{side} #{line.zip(l_labels).map{|c, l| pretty ? c.ljust(l) : c }.join(" | ")} #{side}"
+      end
+      results << " #{sep} "
+      results.map{|s| s.force_encoding('ASCII-8BIT')}.join("\n")
+    end
+
     def summarize()
       files = {}
       MethodInfo.each do |m|
@@ -13,19 +69,23 @@ module PLine
         minfos = files[m.spath] ||= []
         minfos << m
       end
+      labels = ["Line", "Time(usec)", "Source"]
       files.each do |spath, minfos|
         sinfo = SourceInfo.find(spath)
         source = File.readlines(spath)
         minfos.each do |m|
-          @@output.puts()
-          @@output.puts("========== #{m.description} ==========")
-          @@output.puts("line |    usec     | source")
+          contents = []
           sinfo.lines[(m.sline - 1)..(m.eline - 1)].each_with_index do |t, idx|
             line = m.sline + idx
-            @@output.print(sprintf("%5d: %12d: %s", line, t / 1000, source[line - 1]))
+            contents << [line.to_s, (t / 1000).to_s, source[line - 1]]
           end
+          @@output.puts()
+          desc = "   #{m.description}   "
+          msg = build_message(desc, labels, contents)
+          @@output.puts(msg)
         end
       end
     end
   end
 end
+
