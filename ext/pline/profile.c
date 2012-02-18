50d6deca5ef19bf3ea0af1a34b38808f6a707c32
Satoshi Shiba <shiba@rvm.jp>
2012-02-18 17:17:03 +0900

    add experimental code to profile.c

--- ext/pline/profile.c
+++ ext/pline/profile.c
@@ -1,5 +1,49 @@
 static int pline_callback_info(const char **p_srcfile, long *p_line)
 {
+#if 0
+  VALUE thval = rb_thread_current();
+  rb_thread_t *th = DATA_PTR(thval);
+  rb_control_frame_t *cfp = th->cfp;
+  rb_iseq_t *iseq;
+  const char *srcfile;
+  long line = -1, i, pc;
+
+  /* find cfp */
+  while (!RUBY_VM_CONTROL_FRAME_STACK_OVERFLOW_P(th, cfp)) {
+    if (RUBY_VM_NORMAL_ISEQ_P(cfp->iseq)) {
+        break;
+    }
+    cfp = RUBY_VM_PREVIOUS_CONTROL_FRAME(cfp);
+  }
+  if (RUBY_VM_CONTROL_FRAME_STACK_OVERFLOW_P(th, cfp)) {
+    return 0;
+  }
+  iseq = cfp->iseq;
+
+  /* find sourcefile */
+  srcfile = RSTRING_PTR(iseq->filename);
+  if (!srcfile) {
+    return 0;
+  }
+
+  /* find line */
+  if (iseq->insn_info_size <= 0) {
+    return 0;
+  }
+  pc = cfp->pc - iseq->iseq_encoded;
+  for (i = 0; i < iseq->insn_info_size; i++) {
+    if (iseq->insn_info_table[i].position == pc) {
+      line = iseq->insn_info_table[i - 1].line_no;
+      break;
+    }
+  }
+  if (line < 0) {
+    line = iseq->insn_info_table[i - 1].line_no;
+  }
+  if (line < 0) {
+    rb_bug("pline_callback_info: should not be reached");
+  }
+#else
   const char *srcfile;
   long line;
 
@@ -12,6 +56,7 @@ static int pline_callback_info(const char **p_srcfile, long *p_line)
   if (line < 0) {
     return 0;
   }
+#endif
 
   *p_srcfile = srcfile;
   *p_line = line;
