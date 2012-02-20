e237842c9d137934c642b299c35dfa2ef4ae9eb4
Satoshi Shiba <shiba@rvm.jp>
2012-02-20 20:23:00 +0900

    modify build process

--- ext/pline/pline.c.rb
+++ ext/pline/pline.c.rb
@@ -1,14 +1,28 @@
+require "erb"
+
+case RUBY_VERSION
+when "1.9.2"
+  ruby_srcdir = "ruby_source/1.9.2"
+when "1.9.3"
+  ruby_srcdir = "ruby_source/1.9.3"
+else
+  raise("Unsupported ruby version #{RUBY_VERSION}")
+end
+
+ERB.new(DATA.read, 0, "%-").run()
+__END__
+
 #include <ruby.h>
 
-#include "ruby_source/1.9.3/vm_core.h"
-#include "ruby_source/1.9.3/eval_intern.h"
-#include "ruby_source/1.9.3/iseq.h"
-#include "ruby_source/1.9.3/gc.h"
+#include "<%= ruby_srcdir %>/vm_core.h"
+#include "<%= ruby_srcdir %>/eval_intern.h"
+#include "<%= ruby_srcdir %>/iseq.h"
+#include "<%= ruby_srcdir %>/gc.h"
 #include <ruby/vm.h>
 #include <ruby/encoding.h>
-#include "ruby_source/1.9.3/vm_insnhelper.h"
-#include "ruby_source/1.9.3/vm_insnhelper.c"
-#include "ruby_source/1.9.3/vm_exec.h"
+#include "<%= ruby_srcdir %>/vm_insnhelper.h"
+#include "<%= ruby_srcdir %>/vm_insnhelper.c"
+#include "<%= ruby_srcdir %>/vm_exec.h"
 
 #define TRUE 1
 #define FALSE 0
@@ -23,8 +37,8 @@
 #endif
 #define USE_INSN_RET_NUM 1
 
-#include "ruby_source/1.9.3/insns_info.inc"
-#include "ruby_source/1.9.3/manual_update.h"
+#include "<%= ruby_srcdir %>/insns_info.inc"
+#include "<%= ruby_srcdir %>/manual_update.h"
 
 #ifndef _WIN32
 #include <time.h>
